ce17aa786836dceb6bfe7d2da071dd72af95a975
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-10-24 23:49:22 +0330

    basic structure

---
+++ command_help.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error{
+	fmt.Println("Welcome to Pokedex Help menu!")
+	fmt.Println("Available commands: ")
+	availabe_commands := getCommands()
+	for _ , cmd :=  range availabe_commands{
+		fmt.Printf("- %s: %s \n", cmd.name, cmd.description)
+		fmt.Println("")
+	}
+	return  nil
+}
